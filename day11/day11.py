b0820d1627ed7a314a024d575a63f2465679bf12
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2024-12-16 10:56:11 -0500

    day 11 complete

--- day11/day11.py
+++ day11/day11.py
@@ -1,3 +1,20 @@
 def algo(input_list: list, blinks: int) -> list:
-    for stone in input_list:
-        pass
+    current_list = input_list
+    for iter in range(blinks):
+        # print(f'Iteration: {iter}')
+        new_list = []
+        for stone in current_list:
+            # print(f'Current Stone: {stone}')
+            if stone == 0:
+                # print('Rule: is 0')
+                new_list.append(1)
+            elif len(str(stone)) % 2 == 0:
+                # print('Rule: split')
+                split_index = int(len(str(stone)) / 2)
+                new_list += [int(str(stone)[:split_index]),
+                             int(str(stone)[split_index:])]
+            else:
+                # print('Rule: none of the above')
+                new_list.append(stone*2024)
+        current_list = new_list
+    return current_list
--- day11/test_day11.py
+++ day11/test_day11.py
@@ -2,5 +2,14 @@ from day11 import algo
 
 input_test = [125, 17]
 
+
 def test_algo():
     assert algo(input_test, 3) == [512072, 1, 20, 24, 28676032]
+
+
+def test_solve():
+    my_input = [5, 127, 680267, 39260, 0, 26, 3553, 5851995]
+    blinks = 75
+    result = algo(my_input, blinks)
+    print(len(result))
+    assert False
