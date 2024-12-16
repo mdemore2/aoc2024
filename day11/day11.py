57ac0efd6c5c3bc3f9bdc815216ed675f8e091fd
mdemore2 <53943861+mdemore2@users.noreply.github.com>
2024-12-16 12:54:31 -0500

    wip: day 11 pt2

--- day11/day11.py
+++ day11/day11.py
@@ -1,4 +1,6 @@
-def algo(input_list: list, blinks: int) -> list:
+import json
+
+def algo(input_list: list, blinks: int, length_only:bool=False) -> list:
     current_list = input_list
     for iter in range(blinks):
         # print(f'Iteration: {iter}')
@@ -17,4 +19,49 @@ def algo(input_list: list, blinks: int) -> list:
                 # print('Rule: none of the above')
                 new_list.append(stone*2024)
         current_list = new_list
+    if length_only:
+        return len(current_list)
     return current_list
+
+
+#def algo_batched(input_list: list, blinks: int) -> list:
+#    current_list = input_list
+#    current_length = len(input_list)
+#    for iter in range(blinks):
+#        if len(current_list) > 200000:
+#            with open(f"to_process{}.json", "w") as f:
+#                json.dump(current_list[100000:], f)
+#        # print(f'Iteration: {iter}')
+#        new_list = []
+#        for stone in current_list:
+#            # print(f'Current Stone: {stone}')
+#            if stone == 0:
+#                # print('Rule: is 0')
+#                new_list.append(1)
+#            elif len(str(stone)) % 2 == 0:
+#                # print('Rule: split')
+#                split_index = int(len(str(stone)) / 2)
+#                new_list += [int(str(stone)[:split_index]),
+#                             int(str(stone)[split_index:])]
+#            else:
+#                # print('Rule: none of the above')
+#                new_list.append(stone*2024)
+#        current_list = new_list
+#        current_length = len(current_list)
+#    return current_length
+
+def simple_algo(input_list:list) -> list:
+    new_list = []
+    for stone in input_list:
+        if stone == 0:
+            # print('Rule: is 0')
+            new_list.append(1)
+        elif len(str(stone)) % 2 == 0:
+            # print('Rule: split')
+            split_index = int(len(str(stone)) / 2)
+            new_list += [int(str(stone)[:split_index]),
+                         int(str(stone)[split_index:])]
+        else:
+            # print('Rule: none of the above')
+            new_list.append(stone*2024)
+    return new_list
--- day11/test_day11.py
+++ day11/test_day11.py
@@ -1,15 +1,40 @@
 from day11 import algo
-
-input_test = [125, 17]
-
-
-def test_algo():
-    assert algo(input_test, 3) == [512072, 1, 20, 24, 28676032]
+import json
+import gc
 
 
-def test_solve():
+
+#def test_algo():
+#   input_test = [125, 17]
+#    assert algo(input_test, 3) == [512072, 1, 20, 24, 28676032]
+#
+#
+#def test_solve():
+#    my_input = [5, 127, 680267, 39260, 0, 26, 3553, 5851995]
+#    blinks = 25
+#    result = algo(my_input, blinks)
+#    with open('output_25.json','w') as f:
+#        json.dump(result, f)
+#    print(len(result))
+#    assert False
+
+
+def test_solve_part2():
     my_input = [5, 127, 680267, 39260, 0, 26, 3553, 5851995]
     blinks = 75
-    result = algo(my_input, blinks)
-    print(len(result))
+    final_length = 0
+    for stone in my_input:
+        final_length += algo([stone], 75, length_only=True)
+    gc.collect()
+
+    print(final_length)
     assert False
+
+
+
+
+
+    
+
+
+
