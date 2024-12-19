cdacd4db23e56bffe3fb96a17dc2b1bf4318a54d
mdemore2 <53943861+mdemore2@users.noreply.github.com>
2024-12-18 20:30:59 -0500

    day 1, part 1 complete

---
+++ day01/day01.py
@@ -0,0 +1,22 @@
+
+
+def parse_input() -> tuple:
+    input_1 = []
+    input_2 = []
+    with open('input.txt','r') as f:
+        line = f.readline()
+        while line:
+            part_1, part_2 = line.split()
+            input_1.append(int(part_1))
+            input_2.append(int(part_2))
+            line = f.readline()
+    return input_1, input_2
+
+
+def calculate_distance(input_1: list, input_2: list) -> int:
+    input_1.sort()
+    input_2.sort()
+    distance = 0
+    for index in range(len(input_1)):
+        distance += abs(input_1[index] - input_2[index])
+    return distance
---
+++ day01/test_day01.py
@@ -0,0 +1,11 @@
+from day01 import calculate_distance, parse_input
+
+def test_simple():
+    list_1 = [3,4,2,1,3,3]
+    list_2 = [4,3,5,3,9,3]
+    assert calculate_distance(list_1, list_2) == 11
+
+def test_part1():
+    list_1, list_2 = parse_input()
+    print(calculate_distance(list_1, list_2))
+    assert False
