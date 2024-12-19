48f384f2ae621201a9b8ece6ec0069db787092fd
mdemore2 <53943861+mdemore2@users.noreply.github.com>
2024-12-18 20:36:55 -0500

    day 1, part 2 complete

--- day01/day01.py
+++ day01/day01.py
@@ -20,3 +20,10 @@ def calculate_distance(input_1: list, input_2: list) -> int:
     for index in range(len(input_1)):
         distance += abs(input_1[index] - input_2[index])
     return distance
+
+def calculate_similarity(input_1: list, input_2: list) -> int:
+    similarity = 0
+    for num in input_1:
+        appearances = input_2.count(num)
+        similarity += num * appearances
+    return similarity
--- day01/test_day01.py
+++ day01/test_day01.py
@@ -1,4 +1,4 @@
-from day01 import calculate_distance, parse_input
+from day01 import calculate_distance, parse_input, calculate_similarity
 
 def test_simple():
     list_1 = [3,4,2,1,3,3]
@@ -8,4 +8,14 @@ def test_simple():
 def test_part1():
     list_1, list_2 = parse_input()
     print(calculate_distance(list_1, list_2))
+    assert True
+
+def test_simple_part2():
+    list_1 = [3,4,2,1,3,3]
+    list_2 = [4,3,5,3,9,3]
+    assert calculate_similarity(list_1, list_2) == 31
+
+def test_part2():
+    list_1, list_2 = parse_input()
+    print(calculate_similarity(list_1, list_2))
     assert False
