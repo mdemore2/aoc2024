828b1946526b548cdd97f750780794c3c9d497a7
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2024-12-19 12:35:35 -0500

    wip: init day19 approach

--- day19/day19.py
+++ day19/day19.py
@@ -9,5 +9,20 @@ def parse_input(filename:str) -> tuple:
             line = f.readline()
             if line:
                 desired_list.append(line)
-                
+
     return available_list, desired_list
+
+def count_possible(filename:str) -> int:
+    available, desired = parse_input(filename)
+    possible = 0
+    for pattern in desired:
+        avail_index = 0
+        pattern_index = 0
+        while avail_index < len(available):
+            if pattern.startswith(available[avail_index], start=pattern_index, end=len(pattern)):
+                pattern_index += len(available[avail_index])
+                avail_index += 1
+            if pattern_index >= len(pattern):
+                possible += 1
+    return possible
+            
---
+++ day19/test_day19.py
@@ -0,0 +1,2 @@
+def test_example_part1():
+    assert count_possible('example_input.txt') == 6
