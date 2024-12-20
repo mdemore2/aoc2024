db02ece94c5ed6547bca782508fd52bcd575e31c
mdemore2 <53943861+mdemore2@users.noreply.github.com>
2024-12-19 22:26:57 -0500

    wip: day19

--- day19/day19.py
+++ day19/day19.py
@@ -1,9 +1,14 @@
-def parse_input(filename:str) -> tuple:
+import itertools
+
+
+def parse_input(filename: str) -> tuple:
     available_list = []
     desired_list = []
 
     with open(filename, 'r') as f:
         line = f.readline()
+        line = line.strip()
+        line = line.replace(" ", "")
         available_list = line.split(',')
         while line:
             line = f.readline()
@@ -12,17 +17,16 @@ def parse_input(filename:str) -> tuple:
 
     return available_list, desired_list
 
-def count_possible(filename:str) -> int:
+
+def count_possible(filename: str) -> int:
     available, desired = parse_input(filename)
     possible = 0
+    perms = itertools.permutations(available)
+    perms = [''.join(combo) for combo in perms]
+    print(perms)
     for pattern in desired:
-        avail_index = 0
-        pattern_index = 0
-        while avail_index < len(available):
-            if pattern.startswith(available[avail_index], start=pattern_index, end=len(pattern)):
-                pattern_index += len(available[avail_index])
-                avail_index += 1
-            if pattern_index >= len(pattern):
-                possible += 1
+        if any(pattern in substring for substring in perms):
+            print(pattern)
+            possible += 1
+
     return possible
-            
--- day19/test_day19.py
+++ day19/test_day19.py
@@ -1,2 +1,5 @@
+from day19 import count_possible
+
+
 def test_example_part1():
     assert count_possible('example_input.txt') == 6
